22604cb4286baab5b403660887323f151ba52cbc
arnav-alt <69987736+arnav-alt@users.noreply.github.com>
2020-08-21 02:22:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,16 @@
 
 ///Edit Under This only
 
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="/__/firebase/7.18.0/firebase-app.js"></script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="/__/firebase/7.18.0/firebase-analytics.js"></script>
-
-<!-- Initialize Firebase -->
-<script src="/__/firebase/init.js"></script>
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDPtzG09k6gfNcGdR--b6EYSTSLexS8EGc",
+  authDomain: "education-92448.firebaseapp.com",
+  databaseURL: "https://education-92448.firebaseio.com",
+  projectId: "education-92448",
+  storageBucket: "education-92448.appspot.com",
+  messagingSenderId: "828261681329",
+  appId: "1:828261681329:web:becc7a29348e48b73c7a90",
+  measurementId: "G-4LRDF60057"
+};
      
  
 ////Do Not Edit Below This
